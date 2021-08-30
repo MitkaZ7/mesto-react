9f38a25497e46e7fb2f9ff6bf19dfcb8dd3c3470
MitkaS7 <mitka.dev@gmail.com>
2021-08-30 23:05:31 +0300

    fix: make the name of form unique again

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,9 +4,11 @@ function PopupWithForm(props) {
     <div className={`popup popup_type_${props.name} ${props.isOpen ? "popup_status_opened" : ""}`}>
       <div className="popup__container">
         <button className="popup__button-close button" type="button" onClick={props.onClose}></button>
-        <form action="#" name="add-place" className="popup__form popup__form_type_add">
+        <form action="#" name="popup-form" className="popup__form popup__form_type_add">
           <h3 className="popup__title">{props.title}</h3>
-          {props.children}
+          <fieldset className="popup__fieldset">
+            {props.children}
+          </fieldset>
           <button type="submit" className="popup__button-submit button button_type_save">{props.buttonTitle}</button>
         </form>
       </div>
